7cf07395149e60a8184ad4a01ea16c199b674def
Olha Stadnik <olgastadnik2507@gmail.comcd>
2025-03-15 13:38:48 +0200

    register models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserBalance, CurrencyExchange
 
-# Register your models here.
+admin.site.register(UserBalance)
+admin.site.register(CurrencyExchange)
